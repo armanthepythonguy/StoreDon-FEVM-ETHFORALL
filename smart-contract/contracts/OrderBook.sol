49a2f0ab5c9a05334806db73a6d939c655149809
Arman Aurobindo <66505181+armanthepythonguy@users.noreply.github.com>
2023-02-19 23:41:55 +0530

    Update OrderBook.sol

--- smart-contract/contracts/OrderBook.sol
+++ smart-contract/contracts/OrderBook.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9; 
+pragma solidity ^0.8.17;
 
 import { MinerAPI } from "@zondax/filecoin-solidity/contracts/v0.8/MinerAPI.sol";
 import { MinerTypes } from "@zondax/filecoin-solidity/contracts/v0.8/types/MinerTypes.sol";
@@ -13,7 +13,7 @@ contract OrderBook{
     uint64 constant METHOD_SEND = 0;
 
     struct Order {
-        bytes orderId;
+        string orderId;
         string dataCID;
         address bidder;
         address asker;
@@ -44,7 +44,7 @@ contract OrderBook{
     
     mapping (uint256 => Bid) bids;
     mapping (uint256 => Ask) asks;
-    mapping (bytes => Order) orders;
+    mapping (string => Order) orders;
     address owner;
     address[] oracles;
     string[] oraclesIP;
@@ -70,7 +70,7 @@ contract OrderBook{
 
     event NewBidEvent(uint256 bidId, address bidder, uint256 bidAmount, uint256 bidSize);
     event NewAskEvent(uint256 askId, address asker, uint256 askAmount, uint256 askSize);
-    event orderCreatedEvent(bytes orderId, uint256 bidId, address bidder, uint256 askId, address asker, address oracleAddress, string dataCID);
+    event orderCreatedEvent(string orderId, uint256 bidId, address bidder, uint256 askId, address asker, address oracleAddress, string dataCID);
 
     constructor(address[] memory _oracles, string [] memory _oraclesIP){
         owner = msg.sender;
@@ -83,9 +83,14 @@ contract OrderBook{
         oraclesIP.push(_oracleIP);
     }
 
+    function addDemoAsks(address _SPAddress, uint _askAmount, uint256 _askSize) external onlyOwner{
+        asks[askId] = Ask(askId, _SPAddress, _askAmount, _askSize);
+        askId++;
+        emit NewAskEvent(askId-1, _SPAddress, _askAmount, _askSize);
+    }
 
-    function addBid(address _oracleAddress, uint256 _bidAmount, uint256 _bidSize, string memory _dataCID) external payable {
-        require(msg.value >= _bidAmount);
+    function addBid(address _oracleAddress, uint256 _bidAmount, uint256 _bidSize, string memory _dataCID) external payable{
+        require(msg.value >= _bidAmount, "Need more funds");
         bids[bidId] = Bid(bidId, msg.sender, _bidAmount, _bidSize, _oracleAddress, _dataCID);
         bidId++;
         emit NewBidEvent(bidId-1, msg.sender, _bidAmount, _bidSize);
@@ -98,7 +103,7 @@ contract OrderBook{
         emit NewAskEvent(askId-1, msg.sender, _askAmount, _askSize);
     }
 
-    function proveOrder(bytes memory _orderId, uint256 _bidId, uint256 _askId, uint256 _amountFixed, uint256 _sizeFixed) external onlyOracle{
+    function proveOrder(string memory _orderId, uint256 _bidId, uint256 _askId, uint256 _amountFixed, uint256 _sizeFixed) external onlyOracle{
         if(orders[_orderId].amountFixed != 0){
             orders[_orderId].confirmations++;
             if(orders[_orderId].confirmations > oracles.length/2 && orders[_orderId].confirmed==false){
@@ -107,10 +112,15 @@ contract OrderBook{
             }
         }else{
             orders[_orderId] = Order(_orderId, bids[_bidId].dataCID, bids[_bidId].bidder, asks[_askId].asker, _amountFixed, _sizeFixed, bids[_bidId].oracleAddress, 1, false, false);
+            orders[_orderId].confirmations++;
+            if(orders[_orderId].confirmations > oracles.length/2 && orders[_orderId].confirmed==false){
+                orders[_orderId].confirmed = true;
+                emit orderCreatedEvent(_orderId, _bidId, bids[_bidId].bidder, askId, asks[_askId].asker, bids[_bidId].oracleAddress, bids[_bidId].dataCID);
+            }
         }
     }
 
-    function askBounty(bytes memory _orderId, uint64 _actorId) external{
+    function askBounty(string memory _orderId, uint64 _actorId) external{
         require(orders[_orderId].confirmed == true);
         require(orders[_orderId].bountyClaimed == false);
         require(orders[_orderId].asker == msg.sender);
